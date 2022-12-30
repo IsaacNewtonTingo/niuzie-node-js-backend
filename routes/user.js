14688a87c8c702978d627b66794e01c52da65c86
Isaac Newton Tingo <newtone480@gmail.com>
2022-12-30 12:24:04 +0300

    Initializing project

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { signup, verifyCode, login } = require("../controller/user");
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/verify-code", verifyCode);
+router.post("/login", login);
+
+module.exports = router;
