fa724886dfa34dd6bea1be5aa4fe5e0c100b8d4e
Isaac Newton Tingo <newtone480@gmail.com>
2023-01-16 22:38:35 +0300

    Full name

--- controller/general/contact-us.js
+++ controller/general/contact-us.js
@@ -9,14 +9,14 @@ let transporter = nodemailer.createTransport({
 });
 
 exports.contactUs = async (req, res) => {
-  const { email, message, name } = req.body;
+  const { email, message, fullName } = req.body;
 
   const mailOptions = {
     from: process.env.AUTH_EMAIL,
     to: "newtontingo@gmail.com",
     subject: "Customer support",
     html: `<p>
-                <strong>Name: ${name}</strong><br/>
+                <strong>Name: ${fullName}</strong><br/>
                 <strong>Email: ${email}</strong><br/>
                 <strong>Message: ${message}</strong><br/>
             </p>`,
