a90e7889bc9c79df828b7e84c9ee0b356758fa5e
Isaac Newton Tingo <newtone480@gmail.com>
2023-02-04 23:26:03 +0300

    Sort

--- controller/admin/contact-us.js
+++ controller/admin/contact-us.js
@@ -8,10 +8,12 @@ exports.getMessages = async (req, res) => {
   try {
     const user = await User.findOne({ _id: userID });
     if (user.admin == true) {
-      const messages = await ContactUs.find({}).populate({
-        path: "user",
-        select: "firstName lastName phoneNumber profilePicture",
-      });
+      const messages = await ContactUs.find({})
+        .populate({
+          path: "user",
+          select: "firstName lastName phoneNumber profilePicture",
+        })
+        .sort({ createdAt: -1 });
       res.json({
         status: "Success",
         message: "Messages retrieved successfully",
