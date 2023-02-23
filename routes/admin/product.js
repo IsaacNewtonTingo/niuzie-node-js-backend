0039717ffa35f33ec5a7dd8873fb0784566cdfe0
Isaac Newton Tingo <newtone480@gmail.com>
2023-02-23 19:33:14 +0300

    Approve and reject

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -8,9 +8,11 @@ const {
   getNewProducts,
   getApprovedProducts,
   getRejectedProducts,
+  rejectProduct,
 } = require("../../controller/admin/products");
 
 router.put("/approve-product/:id", access, approveProduct);
+router.put("/reject-product/:id", access, rejectProduct);
 router.get("/get-new-products", access, getNewProducts);
 router.get("/get-approved-products", access, getApprovedProducts);
 router.get("/get-rejected-products", access, getRejectedProducts);
